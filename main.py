54dc6666d3bf3effaa8bfc7f03fea11af9c939ed
Spencer Lommel <spencer41965@marvin.com>
2025-07-16 15:53:50 -0500

    keyparse

--- main.py
+++ main.py
@@ -28,8 +28,26 @@ char_map = {
 
 def keyparse(key_string):
     char_stack = []
+
+    expect = None
+    readUntil = None
     for i in key_string:
-        print(i)
+        # Debug print all chars
+        # print(f'"{i}"')
+
+        # Skip whitespacing for now
+        # TODO: We don't always want to skip white spacing. Ex. "Caps Lock" is valid key and contains space
+        if expect is not None:
+            if expect == i:
+                print("correct expected")
+                expect = None
+            else:
+                print("FILE ERR: 003. unexpected char")
+                expect = None
+
+
+        elif i in "\r\n":
+            continue
         if i in "[{":
             print(f'Pushed: {i}')
             char_stack.append(i)
@@ -40,7 +58,13 @@ def keyparse(key_string):
                 char_stack.pop(-1)
 
         else:
-            pass
+            # if previous char is { this is a hwxy modifier so if the proceeding char
+            # isn't in "hwxy" we must throw error
+            if char_stack[-1] == '{':
+                if i not in "hwxy":
+                    print("FILE ERR: 002. Invalid sizing modifier char")
+                else:
+                    expect = ":"
 
     if len(char_stack) > 0:
         print("FILE ERR: 001. Stack not empty")
@@ -50,7 +74,7 @@ def keyparse(key_string):
 # with open("ansi104.kb", 'r', encoding='utf-8') as kb_file:
 #     keyparse(kb_file)
 
-# keyboard_file = """
-# [{w:1.75},"Caps Lock","A","S]"
-# """
-# keyparse(keyboard_file)
+keyboard_file = """
+[{w:1.75},"Caps Lock","A","S]"
+"""
+keyparse(keyboard_file)
